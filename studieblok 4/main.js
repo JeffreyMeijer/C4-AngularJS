07f8984d1130d2c54165c610581eca528e7e9899
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-02-25 09:54:55 +0100

    Studieblok 4 opdracht 5

--- studieblok 4/main.js
+++ studieblok 4/main.js
@@ -1,4 +1,5 @@
 var app = angular.module("myApp", []);
+app.directive("hr", function() {     return {         template : "<h1>Made by a directive!</h1>"     }; });
 app.controller("myCtrl", function($scope) {
   $scope.firstName = "Jeffrey";
   $scope.lastName = "Meijer";
