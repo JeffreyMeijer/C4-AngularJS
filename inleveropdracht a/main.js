4f8fc9f36aca98f5790b9b8692e4f1909b9b3f62
JeffreyMeijer <123974@student.drenthecollege.nl>
2020-02-26 09:13:17 +0100

    Inleveropdracht A

---
+++ inleveropdracht a/main.js
@@ -0,0 +1,10 @@
+var app = angular.module("myApp",[]);
+app.controller("formCtrl", function($scope){
+  $scope.voornaam;
+  $scope.achternaam;
+  $scope.adres;
+  $scope.huisnummer;
+  $scope.postcode;
+  $scope.woonplaats;
+  $scope.email;
+});
